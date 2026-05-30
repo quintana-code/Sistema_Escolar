0b4ec155a4a3ca52f2d4d8b154a8d150ffce079e
Geraline Lizeth <quintana.hernandez.geraline@gmail.com>
2026-05-30 10:20:24 -0600

    Delete modulos/__pycache__ directory
    Elimine caché

--- modulos/__pycache__/materia.py
+++
@@ -1,9 +0,0 @@
-class datos_materia:    
-    def __init__(self, ID_materia, nombre_materia, Usuario_asignado): #el usuario asignado quiere decir quien imparte la materia o quien esta a cargo de ella.
-        self.ID_materia= ID_materia
-        self.nombre_materia = nombre_materia
-        self.Usuario_asignado= Usuario_asignado 
-    def mostrar_MATERIA(self):
-        print ("ID_materia:", self.ID_materia)
-        print("nombre_materia:", self.nombre_materia)
-        print("Usuario_asignado: ", self.Usuario_asignado)
